e2853945d92418a786a3880f0b1fb6e72b2ed359
Shep <alfredochsoliz@gmail.com>
2018-11-24 15:34:45 -0400

    cambiando persona(animales)->Persona(persona)

--- app/src/main/java/bo/edu/uagrm/sarapp/ui/FichaMedicaListFragment.kt
+++ app/src/main/java/bo/edu/uagrm/sarapp/ui/FichaMedicaListFragment.kt
@@ -3,10 +3,10 @@ package bo.edu.uagrm.sarapp.ui
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -19,12 +19,24 @@ import bo.edu.uagrm.sarapp.adapters.PersonaAdapter
 import bo.edu.uagrm.sarapp.data.model.Persona
 import bo.edu.uagrm.sarapp.viewmodels.PersonaViewModel
 import kotlinx.android.synthetic.main.fragment_ficha_medica_list.view.*
+import androidx.core.view.MenuItemCompat.getActionView
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.disposables.Disposable
+import org.reactivestreams.Subscription
+import java.util.concurrent.TimeUnit
+
 
 class FichaMedicaListFragment : Fragment() {
 
+    private val TAG = FichaMedicaListFragment::class.java.canonicalName as String
     private lateinit  var list: RecyclerView;
     private val adapter = PersonaAdapter()
     private lateinit var viewModel: PersonaViewModel
+    private var query:String = "";
+    private lateinit var observer:Observable<String>
+    private var subscription:Disposable?=null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,25 +46,23 @@ class FichaMedicaListFragment : Fragment() {
         list = view.recycler_ficha_medica;
         list.layoutManager = LinearLayoutManager(context);
         // Inflate the layout for this fragment
-
-
+        setHasOptionsMenu(true)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //viewModel = ViewModelProviders.of(this, Injection.provideViewModelFactory(this))
         viewModel = ViewModelProviders.of(this, Injection.provideViewModelFactory(context as Context))
             .get(PersonaViewModel::class.java)
-
         initAdapter()
-        val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
-        //viewModel.searchPersona(query)
-        viewModel.searchPersona("Eugenia")
-        initSearch(query)
+        query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
         
 
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(LAST_SEARCH_QUERY, viewModel.lastQueryValue())
     }
 
     private fun initAdapter(){
@@ -77,6 +87,57 @@ class FichaMedicaListFragment : Fragment() {
 
     companion object {
         private const val LAST_SEARCH_QUERY: String = "last_search_query"
-        private const val DEFAULT_QUERY = "Helen"
+        private const val DEFAULT_QUERY = ""
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.app_bar_ficha_medica,menu)
+
+        //SearchView
+        val mSearch:MenuItem = menu!!.findItem(R.id.action_search)
+        val mSearchView = mSearch.actionView as SearchView
+
+        MenuItemCompat.expandActionView(mSearch);
+        mSearchView.setQuery(query,false)
+
+
+        observer = Observable.create(ObservableOnSubscribe<String> { observer ->
+            mSearchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    observer.onNext(query!!)
+                    //Log.d(TAG,query)
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    observer.onNext(newText!!)
+                    //Log.d(TAG,newText)
+                    return false
+                }
+            })
+        })
+            .map{it->it.toLowerCase().trim()}
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+
+        subscription = observer.subscribe{query ->
+            Log.d(TAG,query)
+            viewModel.searchPersona(query)
+        };
+    }
+
+  /*  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_search) {
+            return true;
+        }
+        return false;
+    }*/
+
+     override fun onDestroyView() {
+        super.onDestroyView()
+         Log.d(TAG,"DISPOSE")
+         if(subscription!=null && !(subscription as Disposable).isDisposed )
+             (subscription as Disposable).dispose()
     }
 }
--- app/src/main/java/bo/edu/uagrm/sarapp/viewmodels/PersonaViewModel.kt
+++ app/src/main/java/bo/edu/uagrm/sarapp/viewmodels/PersonaViewModel.kt
@@ -33,44 +33,7 @@ class PersonaViewModel(private val repository:PersonaRepository) : ViewModel() {
         queryLiveData.postValue(queryString)
     }
     fun lastQueryValue(): String? = queryLiveData.value
-    /*
-    fun getPersonas2(): LiveData<ArrayList<Persona>> {
-        if (mPersonas.value == null) {
-            FirebaseDatabase.getInstance()
-                .getReference("animals")
-                .addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            Log.i("DataSnapshot :",dataSnapshot.value.toString())
-                            var elements = dataSnapshot.children.toMutableList();
-                            var lista: ArrayList<Persona> = ArrayList<Persona>();
-                            //var lista: ArrayList<Persona> = dataSnapshot.value;
-                            /*for(element in dataSnapshot.children){
-                               // val map = element.getValue(Map::class.java) as Map<String, String>
-                                Log.i("Element :",element.value.toString())
 
-                                val persona = element.getValue(Persona::class.java) as Persona
-                                lista.add(persona);
-                                //lista.add(Persona(map.get("nombreCompleto") as String));
-                            }*/
-                            for (element in elements) {
-                                //lista.add(Persona(element.toString()));
-                                var persona: Persona = element.getValue(Persona::class.java) as Persona
-                                lista.add(persona);
-                            }
-                            mPersonas.postValue(lista)
-                        }
-                    }
-
-                    override fun onCancelled(p0: DatabaseError) {
-                        Log.i("PersonalModel","Error")
-                        Log.i("PersonalModel",p0.message)
-                    }
-                })
-        }
-
-        return mPersonas
-    }*/
 
 
 }
