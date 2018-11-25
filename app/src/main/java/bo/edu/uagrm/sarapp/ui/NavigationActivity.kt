f3a3a720767cf7dfd581e2f83d37f74759d4cb96
Shep <alfredochsoliz@gmail.com>
2018-11-24 21:35:06 -0400

    antes de cambiar FichaMedicaFragment

--- app/src/main/java/bo/edu/uagrm/sarapp/ui/NavigationActivity.kt
+++ app/src/main/java/bo/edu/uagrm/sarapp/ui/NavigationActivity.kt
@@ -4,64 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import bo.edu.uagrm.sarapp.R
+import bo.edu.uagrm.sarapp.databinding.ActivityNavigationBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 
 class NavigationActivity : AppCompatActivity() {
-    private var drawerLayout: DrawerLayout? = null
-    private var toolbar: Toolbar? = null
-    private var navController: NavController? = null
-
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
+
+        //setContentView(R.layout.activity_navigation)
+        val binding: ActivityNavigationBinding = DataBindingUtil.setContentView(this,R.layout.activity_navigation)
         FirebaseApp.initializeApp(this)
-        toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
 
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-
-        // Set up Action Bar
-        navController = host.navController
-        setupActionBar(navController as NavController)
-        setupNavigationMenu(navController as NavController)
+        drawerLayout = binding.drawerLayout
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
 
     }
-
-
-    private fun setupActionBar(navController: NavController) {
-        // TODO STEP 9.5 - Have NavigationUI handle what your ActionBar displays
-//        // This allows NavigationUI to decide what label to show in the action bar
-//        // And, since we are passing in drawerLayout, it will also determine whether to
-//        // show the up arrow or drawer menu icon
-        drawerLayout = findViewById(R.id.drawer_layout)
-        setupActionBarWithNavController(navController, drawerLayout)
-        // TODO END STEP 9.5
-    }
-
-
-    private fun setupNavigationMenu(navController: NavController) {
-        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
-        sideNavView?.setupWithNavController(navController)
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
                 || super.onOptionsItemSelected(item)
     }
-
     override fun onSupportNavigateUp(): Boolean {
-        return drawerLayout.navigateUp(findNavController(R.id.nav_host_fragment))
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
